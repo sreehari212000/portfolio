42c42d692ae3471e9bdade48d89724a6f754f13d
sreehari21 <sreeharijayaram21@gmail.com>
2025-01-29 22:32:59 +0530

    updated resume

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import {BsFilePersonFill, BsFillArrowUpSquareFill} from "react-icons/bs"
 import {FiTwitter} from "react-icons/fi"
 import {Link, } from "react-scroll"
 import LOGO from "../Assets/Sj.png"
-import RESUME from "../Assets/SREEHARI_JAYARAM.pdf"
+import RESUME from "../Assets/SREEHARI_J.pdf"
 
 
 const Navbar = () => {
